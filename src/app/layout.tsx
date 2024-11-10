d804541b3570509db9503ec82505084a7b6182f8
waleedkhalid777 <wk4721260@gmail.com>
2024-11-10 13:39:56 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+   
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black  `}
       >
         {children}
       </body>
